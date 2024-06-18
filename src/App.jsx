a50238d7184c976cc9c6484c33353545c770a015
Your Name <you@example.com>
2024-06-18 13:15:55 +0100

    Website complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import moment from 'moment-timezone'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    const londonTime = moment().tz('Europe/London').format('HH:mm');
+    const newYorkTime = moment().tz('America/New_York').format('HH:mm');
+    const tokyoTime = moment().tz('Asia/Tokyo').format('HH:mm');
+    const sydneyTime = moment().tz('Australia/Sydney').format('HH:mm');
+    const shanghaiTime = moment().tz('Asia/Shanghai').format('HH:mm');
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Some Global Time Zones</h1>
+      <h2>The time in London is: {londonTime}</h2>
+      <h2>The time in New York is: {newYorkTime}</h2>
+      <h2>The time in Tokyo is: {tokyoTime}</h2>
+      <h2>The time in Sydney is: {sydneyTime}</h2>
+      <h2>The time in Shanghai is: {shanghaiTime}</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
